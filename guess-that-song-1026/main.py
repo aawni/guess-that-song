ae909998ba9e0c2b6377a80be6253f94b15d09a2
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-03 21:23:33 -0500

    made more songs play, some still wont for some reason

--- guess-that-song-1026/main.py
+++ guess-that-song-1026/main.py
@@ -30,16 +30,15 @@ class Song(ndb.Model):
     title = ndb.StringProperty(required=True)
     artist = ndb.StringProperty(required=True)
 
-hip_hop_songs=[Song(source="songs/Fashion Killa.mp3", title="Fashion Killa", artist="A$AP ROCKY"),
-               Song(source="songs/Alright.mp3", title="Alright", artist="Kendrick Lamar"),
-               Song(source="songs/Commas.mp3", title="Commas", artist="Future"),
-               Song(source="songs/Good Life.mp3", title="Good Life", artist="Kanye West"),
-               Song(source="songs/Love Sosa.mp3", title="Love Sosa", artist="Chief Keef"),
-               Song(source="songs/Forbidden Fruit.mp3", title="Forbidden Fruit", artist="Kendrick Lamar"),
-               Song(source="songs/My Way.mp3", title="My Way", artist="Fetty Wap")
-               Song(source="songs/Planes.mp3", title="Planes", artist="Jeremih")
-               Song(source="songs/Versace.mp3", title="Versace", artist="Migos")
-                ]
+hiphop_songs=[Song(source="songs/hiphop/Fashion_Killa.mp3", title="Fashion Killa", artist="A$AP ROCKY"),
+               Song(source="songs/hiphop/Alright.mp3", title="Alright", artist="Kendrick Lamar"),
+               Song(source="songs/hiphop/Commas.mp3", title="Commas", artist="Future"),
+               Song(source="songs/hiphop/Good_Life.mp3", title="Good Life", artist="Kanye West"),
+               Song(source="songs/hiphop/Love_Sosa.mp3", title="Love Sosa", artist="Chief Keef"),
+               Song(source="songs/hiphop/Forbidden_Fruit.mp3", title="Forbidden Fruit", artist="Kendrick Lamar"),
+               Song(source="songs/hiphop/My_Way.mp3", title="My Way", artist="Fetty Wap"),
+               Song(source="songs/hiphop/Planes.mp3", title="Planes", artist="Jeremih"),
+               Song(source="songs/hiphop/Versace.mp3", title="Versace", artist="Migos")]
 genres={"hiphop":hiphop_songs}
 
 
@@ -64,11 +63,11 @@ class ResultsHandler(webapp2.RequestHandler):
         counter=1
         print genres[genre]
         for song in genres[genre]:
-
             artist_answer=self.request.get("artist"+str(counter)).lower()
             song_answer=self.request.get("song_title"+str(counter)).lower()
-            if artist_answer==genres[genre][counter-1].artist.lower() and song_answer==genres[genre][counter-1].title.lower():
-                amount_right+=1
+            if artist_answer!="" and song_answer!="":
+                if artist_answer==genres[genre][counter-1].artist.lower() and song_answer==genres[genre][counter-1].title.lower():
+                    amount_right+=1
 
         template_values = {"amount_right": amount_right}
         template = JINJA_ENVIRONMENT.get_template('templates/results.html')
