bb8e80fe43052d607d7fd34d846d0bb3160b5440
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-05 17:07:25 -0500

    added friends

--- guess-that-song-1026/scripts/script.js
+++ guess-that-song-1026/scripts/script.js
@@ -10,21 +10,23 @@ function Verify(){
   {
     $("#error").text(" ");
     $("#error").fadeOut();
-    Verify_Unique_Send(nickname);
+    return Verify_Unique_Send(nickname);
   }
 }
 
 function Verify_Unique_Send(nickname) {
   $.post("/searchnickname", {"nickname": nickname}, function(data){
     if (data.is_unique){
+      alert("It is unique")
       return true;
     }
-    else (
+    else {
+      alert("it is not unique!")
       $("#not_unique_error").text("That nickname is taken. Please enter another!");
       $("#not_unique_error").fadeIn();
       return false;
-    )
-  })
+    }
+  });
 }
 
 function Show_Add_Friends() {
@@ -36,17 +38,9 @@ function Show_Add_Friends() {
 $(document).ready(
   function() {
     $('#setup_form').on('submit', Verify)
-    $('#show_add_friends').click(Show_Add_Friends)
+    $('#show_add_friends').on('submit', Show_Add_Friends)
   }
 );
 $('#imageTag').click(function() {
   $("#youTUBE").attr('src', $("#videoContainer iframe", parent).attr('src') + '?autoplay=0');
 });
-
-// jQuery('a.introVid').click(function(){
-//   autoPlayVideo(  "#videoContainer",'450','283');
-// });
-// function autoPlayVideo(vcode, width, height){
-//   "use strict";
-//   $("#videoContainer").html('<iframe width="'+width+'" height="'+height+'" src="https://www.youtube.com/embed/'+vcode+'?autoplay=1&loop=1&rel=0&wmode=transparent" frameborder="0" allowfullscreen wmode="Opaque"></iframe>');
-// }
