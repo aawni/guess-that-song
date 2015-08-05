9c1a3858096f021d86af3baeeb63fe29bedc681f
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-05 17:18:39 -0500

    changed background

--- guess-that-song-1026/scripts/script.js
+++ guess-that-song-1026/scripts/script.js
@@ -17,11 +17,9 @@ function Verify(){
 function Verify_Unique_Send(nickname) {
   $.post("/searchnickname", {"nickname": nickname}, function(data){
     if (data.is_unique){
-      alert("It is unique")
       return true;
     }
     else {
-      alert("it is not unique!")
       $("#not_unique_error").text("That nickname is taken. Please enter another!");
       $("#not_unique_error").fadeIn();
       return false;
