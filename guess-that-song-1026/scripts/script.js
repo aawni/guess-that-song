bf7d26d9b987631922e61bdec54cab4a166370de
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-05 07:12:52 -0500

    working to add jquery

--- guess-that-song-1026/scripts/script.js
+++ guess-that-song-1026/scripts/script.js
@@ -1,10 +1,21 @@
 function Verify(){
-    $("#results").fadeIn();
+  var nickname = $("#nickname").val();
+  if (nickname == "")
+  {
+    $("#error").text("Please provide a nickname!");
+    $("#error").fadeIn();
     return false;
-}
+  }
+  else
+  {
+    $("#error").text(" ");
+    $("#error").fadeOut();
+    return true;
+    window.location= "/quiz";
+  }
 
 $(document).ready(
   function() {
-    $('#quiz_form').on('submit', Verify)
+    $('#setup_form').on('submit', Verify)
   }
 );
