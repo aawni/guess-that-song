29035bd10c9696eb95eca4de3533902da4fcdc79
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-05 13:06:49 -0500

    doff

--- guess-that-song-1026/scripts/script.js
+++ guess-that-song-1026/scripts/script.js
@@ -21,3 +21,14 @@ $(document).ready(
     $('#setup_form').on('submit', Verify)
   }
 );
+$('#imageTag').click(function() {
+  $("#youTUBE").attr('src', $("#videoContainer iframe", parent).attr('src') + '?autoplay=0');
+});
+
+// jQuery('a.introVid').click(function(){
+//   autoPlayVideo(  "#videoContainer",'450','283');
+// });
+// function autoPlayVideo(vcode, width, height){
+//   "use strict";
+//   $("#videoContainer").html('<iframe width="'+width+'" height="'+height+'" src="https://www.youtube.com/embed/'+vcode+'?autoplay=1&loop=1&rel=0&wmode=transparent" frameborder="0" allowfullscreen wmode="Opaque"></iframe>');
+// }
