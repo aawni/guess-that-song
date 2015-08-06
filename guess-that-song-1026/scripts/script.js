7c33e13c4fd73a02a85d552a8869eb2bd6e7e259
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-06 00:16:50 -0500

    added more styling

--- guess-that-song-1026/scripts/script.js
+++ guess-that-song-1026/scripts/script.js
@@ -1,29 +1,36 @@
 function Verify(){
   var nickname = $("#nickname").val();
-  if (nickname == "")
-  {
-    $("#error").text("Please provide a nickname!");
-    $("#error").fadeIn();
-    return false;
+  var is_new_user = $("#is_new_user").val();
+  if (is_new_user){
+    if (nickname == "")
+    {
+      $("#error").text("Please provide a nickname!");
+      $("#error").fadeIn();
+      return false;
+    }
+    else
+    {
+      $("#error").text(" ");
+      $("#error").fadeOut();
+      return Verify_Unique(nickname);
+    }
   }
-  else
-  {
-    $("#error").text(" ");
-    $("#error").fadeOut();
-    $.post("/searchnickname", {"nickname": nickname}, function(data){
-      if (data.is_unique){
-        alert("it is unique!")
-        return true;
-      }
-      else {
-        alert("it is not unique!")
-        $("#not_unique_error").text("That nickname is taken. Please enter another!");
-        $("#not_unique_error").fadeIn();
-        return false;
+}
 
-      }
-    });
-  }
+function Verify_Unique(nickname){
+  $.post("/searchnickname", {"nickname": nickname}, function(data){
+    if (data.is_unique){
+      alert("it is unique!")
+      return true;
+    }
+    else {
+      alert("it is not unique!")
+      $("#not_unique_error").text("That nickname is taken. Please enter another!");
+      $("#not_unique_error").fadeIn();
+      return false;
+
+    }
+  });
 }
 
 function Show_Add_Friends() {
